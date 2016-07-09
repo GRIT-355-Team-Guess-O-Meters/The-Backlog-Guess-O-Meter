feb659c63d40817dcb59635ec27da81c911ae9f0
Sahba Bahizad <sarahjune919@gmail.com>
2016-07-08 17:00:46 -0700

    working on the switch

--- Sprint 1/guess-o-meter/includes/admin/show-table.inc.php
+++ Sprint 1/guess-o-meter/includes/admin/show-table.inc.php
@@ -10,7 +10,27 @@
 
   foreach ($result as $row) {
 //get the status 
+//if($status = "Open") {
 
+/*echo '<div class="switch">
+    <label>
+      Off
+      <input type="checkbox" checked>
+      <span class="lever"></span>
+      On
+    </label>
+  </div>
+';
+}else{
+echo '<div class="switch">
+    <label>
+      Off
+      <input type="checkbox">
+      <span class="lever"></span>
+      On
+    </label>
+  </div>';
+}*/
 $status = $row['status'];
 
     echo
@@ -21,7 +41,12 @@ $status = $row['status'];
 
       <td>".$row['date_created']."</td>
 
-      <td><a class='waves-effect waves-light btn lime darken-4' href='features.php#/" . $row['project_id'] . "'>".$row['status']."</a></td>
+      <td><a class='waves-effect waves-light btn lime darken-4' href='features.php#/" . $row['project_id'] . "'>
+$status
+
+
+
+</a></td>
 
     </tr>";
 
