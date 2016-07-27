7b0fcfb45e0dbd7ef88ddec10594b26c52296fb4
HillaryW <hillmariewag@gmail.com>
2016-07-26 17:23:50 -0700

    added image to splash page

--- Sprint 2/guess-o-meter/index.php
+++ Sprint 2/guess-o-meter/index.php
@@ -4,18 +4,13 @@
 
         <h1>Fast Estimation</h1>
 
-        <img src="includes/images/" alt="Fast Agile Image"/>
+        <img src="./includes/images/FASTagile.jpg" alt="Fast Agile Image"/>
 
         <h3>About Fast Estimation</h3>
 
-        <p> Dragée bonbon sesame snaps marzipan liquorice toffee.
-        Sesame snaps apple pie marshmallow ice cream sweet roll fruitcake donut marshmallow chocolate cake.
-        Ice cream chocolate ice cream. Sweet sweet fruitcake sugar plum lemon drops carrot cake macaroon tiramisu topping.
-        Dragée lemon drops wafer caramels pudding dragée pie tiramisu cake. Biscuit toffee marzipan jujubes.
-        Tart cotton candy pastry biscuit danish ice cream cheesecake chocolate bar.
-        Croissant powder toffee bear claw cheesecake gummies icing. Lollipop ice cream halvah sesame snaps tart
-        ice cream pudding toffee. Croissant cupcake sweet jelly beans powder. Pastry gingerbread carrot cake.
-        Danish chocolate bar macaroon oat cake halvah apple pie cotton candy powder. </p>
+        <p> Current popular agile estimation methods are fraught with many issues.
+        FAST Forecasting is an ultra lightweight alternative to story pointing, story sizing,
+        velocity, planning poker, modified Fibonacci sequence and all the confusion they come with. </p>
 
 
 
